311461fbba84702e1506a4f47a581ef978a94b07
Arthur Teixeira <tuzinf5@gmail.com>
2021-11-09 13:25:15 -0300

    Criando arquivo Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Arthur Aparecido Ferreira Teixeira</h1>
+        </header>
+      </>
+    );
+  }
+}
+
+export default Home;
