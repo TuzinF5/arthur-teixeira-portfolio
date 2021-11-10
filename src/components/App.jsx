7b052550c2b1072fe41ae688b348c07e3a7740e7
Arthur Teixeira <tuzinf5@gmail.com>
2021-11-10 11:05:44 -0300

    Apagando rota

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import NotFound from '../pages/NotFound';
 
 class App extends React.Component {
   render() {
@@ -9,7 +8,6 @@ class App extends React.Component {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     );
