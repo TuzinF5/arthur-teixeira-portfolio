6cf487e515cf3fc84512b0e14ebb8e34ddc1c397
Arthur Teixeira <tuzinf5@gmail.com>
2021-11-10 11:05:18 -0300

    Mudando estilizações

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import style from '../styles/Home.module.css';
 
 class Home extends React.Component {
   render() {
     return (
-      <>
+      <div className={style.home}>
         <header>
           <h1>Arthur Aparecido Ferreira Teixeira</h1>
         </header>
-      </>
+      </div>
     );
   }
 }
