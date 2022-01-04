2244360731ef5610ccbaf4e441e7a56836c06ba8
Arthur Teixeira <tuzinf5@gmail.com>
2022-01-04 10:21:01 -0300

    Adcionando conteúdo inicial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from '../styles/Home.module.css';
+import imageArthur from '../images/Arthur.jpg';
 
 class Home extends React.Component {
   render() {
     return (
       <div className={ style.home }>
         <header>
-          <h1>Arthur Aparecido Ferreira Teixeira</h1>
+          <div>
+            <h1>Arthur Aparecido Ferreira Teixeira</h1>
+          </div>
+          <nav>
+            <Link to="/about">Sobre mim</Link>
+            <Link to="/projects">Projetos</Link>
+          </nav>
         </header>
+
+        <main>
+          <div>
+            <img src={ imageArthur } alt="Arthur sorrindo" />
+          </div>
+          <section>
+            <p>
+              Olá, me chamo Arthur, tenho 23 anos.
+              <br />
+              Sou natural de Campinas - SP,
+              mas atualmente moro em São Gonçalo do Rio Preto - MG.
+              <br />
+              Gosto de
+              jogos e estudar, tenho facilidade em ajudar e ser atencioso com as
+              pessoas.
+              <br />
+              Tenho boa criatividade quanto a pensar e também de
+              resolver problemas.
+            </p>
+          </section>
+        </main>
+
+        <footer>
+          <h1>Feito por Arthur Teixeira</h1>
+        </footer>
       </div>
     );
   }
