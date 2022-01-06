af75feea23d16cbf3873fe750ac62b3d71cfac19
Arthur Teixeira <tuzinf5@gmail.com>
2022-01-06 13:03:06 -0300

    Criando página Home

---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+
+        {/* <main>
+          <div>
+          </div>
+          <section>
+            <p>
+              Olá, me chamo Arthur, tenho 23 anos.
+              <br />
+              Sou natural de Campinas - SP, mas atualmente moro em São Gonçalo
+              do Rio Preto - MG.
+              <br />
+              Gosto de jogos e estudar, tenho facilidade em ajudar e ser
+              atencioso com as pessoas.
+              <br />
+              Tenho boa criatividade quanto a pensar e também de resolver
+              problemas.
+            </p>
+          </section>
+        </main> */}
+
+        {/* <footer>
+          <h1>Feito por Arthur Teixeira</h1>
+        </footer> */}
+      </div>
+    );
+  }
+}
