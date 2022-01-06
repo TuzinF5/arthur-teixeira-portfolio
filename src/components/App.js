861d128cac093677bb59058527155c9e00e5a76b
Arthur Teixeira <tuzinf5@gmail.com>
2022-01-06 13:48:58 -0300

    Criando rotas para as novas três páginas

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from '../pages/About';
 import Home from '../pages/Home';
+import Portfolio from '../pages/Portfolio';
+import Resume from '../pages/Resume';
+import Services from '../pages/Services';
 
 class App extends React.Component {
   render() {
@@ -10,6 +13,9 @@ class App extends React.Component {
         <Routes>
           <Route path="/" element={ <Home /> } />
           <Route path="/about" element={ <About /> } />
+          <Route path="/portfolio" element={ <Portfolio /> } />
+          <Route path="/resume" element={ <Resume /> } />
+          <Route path="/services" element={ <Services /> } />
         </Routes>
       </BrowserRouter>
     );
