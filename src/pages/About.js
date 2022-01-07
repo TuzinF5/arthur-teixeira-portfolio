36c1b6d95eb943c08f04bb9455a75bdda5dd64d3
Arthur Teixeira <tuzinf5@gmail.com>
2022-01-07 17:25:07 -0300

    Adcionando contéudo

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,6 +7,18 @@ export default class About extends Component {
     return (
       <>
         <Header />
+        {/* <p>
+              Olá, me chamo Arthur, tenho 23 anos.
+              <br />
+              Sou natural de Campinas - SP, mas atualmente moro em São Gonçalo
+              do Rio Preto - MG.
+              <br />
+              Gosto de jogos e estudar, tenho facilidade em ajudar e ser
+              atencioso com as pessoas.
+              <br />
+              Tenho boa criatividade quanto a pensar e também de resolver
+              problemas.
+            </p> */}
         <Footer />
       </>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-// import style from '../styles/Home.module.css';
+import style from '../styles/Home.module.css';
 
 export default class Home extends React.Component {
   render() {
     return (
       <>
         <Header />
-        {/* <main className={ style.home }>
+        <main className={ style.home }>
           <section>
-            <p>
-              Olá, me chamo Arthur, tenho 23 anos.
-              <br />
-              Sou natural de Campinas - SP, mas atualmente moro em São Gonçalo
-              do Rio Preto - MG.
-              <br />
-              Gosto de jogos e estudar, tenho facilidade em ajudar e ser
-              atencioso com as pessoas.
-              <br />
-              Tenho boa criatividade quanto a pensar e também de resolver
-              problemas.
-            </p>
+            <p>Arthur</p>
+            <p>Teixeira</p>
+            <p>Uma forte presença online</p>
           </section>
-        </main> */}
+        </main>
         <Footer />
       </>
     );
