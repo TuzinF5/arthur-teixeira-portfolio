7046ea1fa038b09bc19d44c54dc55354052162b8
Arthur Teixeira <tuzinf5@gmail.com>
2022-01-08 22:33:01 -0300

    Adcionando conteúdo a página About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,37 @@
+/* eslint-disable react/jsx-max-depth */
 import React, { Component } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import style from '../styles/About.module.css';
+import imageArthur from '../images/Arthur.jpg';
+import RevealMe from '../components/RevealMe';
 
 export default class About extends Component {
   render() {
     return (
       <>
         <Header />
-        {/* <p>
-              Olá, me chamo Arthur, tenho 23 anos.
-              <br />
-              Sou natural de Campinas - SP, mas atualmente moro em São Gonçalo
-              do Rio Preto - MG.
-              <br />
-              Gosto de jogos e estudar, tenho facilidade em ajudar e ser
-              atencioso com as pessoas.
-              <br />
-              Tenho boa criatividade quanto a pensar e também de resolver
-              problemas.
-            </p> */}
+        <RevealMe>
+          <main className={ style.about }>
+            <section>
+              <div>
+                <img src={ imageArthur } alt="Arthur Teixeira sorrindo" />
+              </div>
+              <div>
+                <RevealMe>
+                  <p>Minha história</p>
+                  <p>
+                    Olá, me chamo Arthur, tenho 23 anos. Sou natural de Campinas
+                    - SP, mas atualmente moro em São Gonçalo do Rio Preto - MG.
+                    Gosto de jogos e estudar, tenho facilidade em ajudar e ser
+                    atencioso com as pessoas. Tenho boa criatividade quanto a
+                    pensar e também de resolver problemas.
+                  </p>
+                </RevealMe>
+              </div>
+            </section>
+          </main>
+        </RevealMe>
         <Footer />
       </>
     );
