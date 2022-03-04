f2d1d7a1770d70cc8d393a92ed0f26259acf47ff
Arthur Teixeira <tuzinf5@gmail.com>
2022-03-04 14:06:05 -0300

    Tirando imagem da página About

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import RevealMe from '../components/RevealMe';
-import imageArthur from '../images/Arthur.jpg';
 import style from '../styles/About.module.css';
 
 export default class About extends Component {
@@ -13,24 +12,16 @@ export default class About extends Component {
         <Header />
         <main className={ style.about }>
           <section>
-            <div>
-              <img src={ imageArthur } alt="Arthur Teixeira sorrindo" />
-            </div>
-            <div>
-              <RevealMe
-                tag="div"
-                config={ { distance: '100px', duration: 2000 } }
-              >
-                <p>Minha história</p>
-                <p>
-                  Olá, me chamo Arthur, tenho 24 anos. Sou natural de Campinas -
-                  SP, mas atualmente moro em São Gonçalo do Rio Preto - MG.
-                  Gosto de jogos e estudar, tenho facilidade em ajudar e ser
-                  atencioso com as pessoas. Tenho boa criatividade quanto em
-                  pensar e também de resolver problemas.
-                </p>
-              </RevealMe>
-            </div>
+            <RevealMe tag="div" config={ { distance: '100px', duration: 2000 } }>
+              <p>Minha história</p>
+              <p>
+                Olá, me chamo Arthur, tenho 24 anos. Sou natural de Campinas -
+                SP, mas atualmente moro em São Gonçalo do Rio Preto - MG. Gosto
+                de jogos e estudar, tenho facilidade em ajudar e ser atencioso
+                com as pessoas. Tenho boa criatividade quanto em pensar e também
+                de resolver problemas.
+              </p>
+            </RevealMe>
           </section>
         </main>
         <Footer />
